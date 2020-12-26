f411b8337314c35d120a9f96f3f8ade0d8fc120c
blast <blast@pop-os.localdomain>
2020-12-26 19:58:10 +0800

    (Func)Add readtxt

--- remote_main.py
+++ remote_main.py
@@ -1,1 +1,7 @@
-if __name__ == "__main__":
+def readtxt(path):
+    with open(path,"r") as f:
+        result=f.read()
+    return result
+
+if __name__ == "__main__": 
+    txt=readtxt("./output.txt")
