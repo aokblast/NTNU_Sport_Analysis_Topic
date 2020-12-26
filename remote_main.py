7f5295cbdddb57de787bd4e39675ddfe42e43339
blast <blast@pop-os.localdomain>
2020-12-26 20:04:55 +0800

    (func)add txt_to_array

--- remote_main.py
+++ remote_main.py
@@ -3,5 +3,14 @@ def readtxt(path):
         result=f.read()
     return result
 
+def txt_to_array(txt):
+    plaintext=txt.split("<my_cutting_tag>")[:-1]
+    resultLIST=[]
+    for i in plaintext:
+        resultLIST.append(eval(i))
+    return resultLIST
+
 if __name__ == "__main__": 
     txt=readtxt("./output.txt")
+    txt=txt_to_array(txt)
+    
